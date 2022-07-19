639ee85fdf5c2be114a519038ec37e1f9ba1594e
geo <tp53.geo@gmail.com>
2022-07-19 02:27:16 +0200

    correction bug admin empechant d'ouvrir un bill si plusieurs listes sont affichées

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -131,6 +131,7 @@ export default class {
   }
 
   handleShowTickets(e, bills, index) {
+    console.log(e, index)
     if (this.counter === undefined || this.index !== index) this.counter = 0
     if (this.index === undefined || this.index !== index) this.index = index
     if (this.counter % 2 === 0) {
@@ -146,7 +147,7 @@ export default class {
     }
 
     bills.forEach(bill => {
-      $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
+      $(`#status-bills-container${this.index} #open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
     })
 
     return bills
